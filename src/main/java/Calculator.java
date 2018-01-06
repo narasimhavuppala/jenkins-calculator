c5aaac98b564b0ed05a566bf35100fd873ba49cf
narasimhavuppala <nraovuppala@gmail.com>
2018-01-06 20:30:18 +0530

    Initital commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,26 @@
+
+public class Calculator {
+	
+	int a;
+	int b;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	
+	int add() {
+		
+		return (this.a + this.b);
+	}
+	
+	
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class CalculatorTest {
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testAdd() {
+		Calculator obj=new Calculator();
+		obj.setA(5);
+		obj.setB(3);
+		Assert.assertEquals(8, obj.add());
+	}
+
+}
