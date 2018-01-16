c0f6d680dc45108aa231849affdc40dc69069cf0
narasimhavuppala <nraovuppala@gmail.com>
2018-01-16 20:21:18 +0530

    Division is daded

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,32 +1,38 @@
 
 public class Calculator {
-	
+
 	int a;
 	int b;
+
 	public int getA() {
 		return a;
 	}
+
 	public void setA(int a) {
 		this.a = a;
 	}
+
 	public int getB() {
 		return b;
 	}
+
 	public void setB(int b) {
 		this.b = b;
 	}
-	
+
 	int add() {
-		
+
 		return (this.a + this.b);
 	}
-	
 
 	int subtract() {
-		
+
 		return (this.a - this.b);
 	}
-	
-	
+
+	int division() {
+
+		return (this.a / this.b);
+	}
 
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,18 +7,33 @@ public class CalculatorTest {
 	@SuppressWarnings("deprecation")
 	@Test
 	public void testAdd() {
-		Calculator obj=new Calculator();
+		Calculator obj = new Calculator();
 		obj.setA(5);
 		obj.setB(3);
 		Assert.assertEquals(8, obj.add());
 	}
-	
+
 	@Test
 	public void testSubtract() {
-		Calculator obj=new Calculator();
+		Calculator obj = new Calculator();
 		obj.setA(5);
 		obj.setB(3);
-		Assert.assertEquals(4, obj.subtract());
+		Assert.assertEquals(2, obj.subtract());
+	}
+
+	@Test(expected = Exception.class)
+	public void testDivision_zero() {
+		Calculator obj = new Calculator();
+		obj.setA(5);
+		obj.setB(0);
+	}
+
+	@Test
+	public void testDivision_NoZero() {
+		Calculator obj = new Calculator();
+		obj.setA(5);
+		obj.setB(1);
+		Assert.assertEquals(5, obj.division());
 	}
 
 }
